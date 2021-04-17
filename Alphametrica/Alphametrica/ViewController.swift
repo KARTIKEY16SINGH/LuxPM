5435d634fb836c5b0a424d35306beba728320660
Kartikey Singh <kartikeysingh16@gmail.com>
2021-04-17 11:55:27 +0530

    [Alphametrica] Created Project in Xcode

---
+++ Alphametrica/Alphametrica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alphametrica
+//
+//  Created by Iron Man on 17/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
