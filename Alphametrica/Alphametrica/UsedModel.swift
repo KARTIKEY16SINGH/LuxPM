7c2eefa7657591bf4175eaed80b2f1808084129f
Kartikey Singh <kartikeysingh16@gmail.com>
2021-04-17 12:05:08 +0530

    [Alphametrica] Added Model For SignUp Module

---
+++ Alphametrica/Alphametrica/UsedModel.swift
@@ -0,0 +1,16 @@
+//
+//  UsedModel.swift
+//  Alphametrica
+//
+//  Created by Iron Man on 17/04/21.
+//
+
+import Foundation
+
+struct UserModel {
+    var name : String
+    var emailId : String
+    var phone : String
+    var password : String
+    var dob : Date
+}
